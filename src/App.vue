6beba6bdcee469bc14c3468ee2e8cf2114c7a1d7
Andres <amontbrun@yahoo.com>
2018-04-11 18:21:52 -0400

    ya funciona active class
    class active en menu

--- src/App.vue
+++ src/App.vue
@@ -9,23 +9,21 @@
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
-      <li class="nav-item active"><router-link to="/" class="nav-link">Home</router-link></li>
-    <li class="nav-item"><router-link to="/peliculas" class="nav-link">Peliculas</router-link></li>
-    <li class="nav-item"><router-link to="/vehiculos" class="nav-link">Vehiculos</router-link></li>
-    <li class="nav-item"><router-link to="/personajes" class="nav-link">Personajes</router-link></li>
-    <li class="nav-item"><router-link to="/planetas" class="nav-link">Planetas</router-link></li>
-    <li class="nav-item"><router-link to="/cruceros" class="nav-link">Cruceros</router-link></li>
+      <li class="nav-item" :class="$route.path == '/' ? 'active':''"><router-link to="/" class="nav-link">Home</router-link></li>
+    <li class="nav-item" :class="$route.path == '/peliculas' ? 'active':''"><router-link to="/peliculas" class="nav-link">Peliculas</router-link></li>
+    <li class="nav-item" :class="$route.path == '/vehiculos' ? 'active':''"><router-link to="/vehiculos" class="nav-link">Vehiculos</router-link></li>
+    <li class="nav-item" :class="$route.path == '/personajes' ? 'active':''"><router-link to="/personajes" class="nav-link">Personajes</router-link></li>
+    <li class="nav-item" :class="$route.path == '/planetas' ? 'active':''"><router-link to="/planetas" class="nav-link">Planetas</router-link></li>
+    <li class="nav-item" :class="$route.path == '/cruceros' ? 'active':''"><router-link to="/cruceros" class="nav-link">Cruceros</router-link></li>
       <li class="nav-item">
         <a class="nav-link disabled" href="#">Disabled</a>
       </li>
     </ul>
   </div>
 </nav>
-
     <router-view></router-view>
 
   </div>
-
 
 </template>
 
