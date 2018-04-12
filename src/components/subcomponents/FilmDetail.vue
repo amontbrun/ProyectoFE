116c89c66ceb8df2cdf8f960fb2e7722ac1642df
Andres <amontbrun@yahoo.com>
2018-04-12 18:27:00 -0400

    adicion de subcomponentes

---
+++ src/components/subcomponents/FilmDetail.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="row">
+    <h4>{{persona.name}}</h4>
+</div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  name: 'Personaje',
+  props: ['url'],
+mounted(){
+ this.getPersona()
+},
+data(){
+    return{
+        persona:{}
+    }
+},
+
+methods: {
+    getPersona(){
+        axios.get(this.url)
+            .then((res)=>{
+                this.persona= res.data
+                console.log(res.data.name)
+
+        })
+            .catch((err)=>{
+                console.log(err)
+            })
+    }
+
+}
+}
+
+</script>
+<style scoped>
+
+</style>
